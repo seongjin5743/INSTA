ffe0929f939af0db583155548742d9580efd83bb
seongjin <seongjin5743@naver.com>
2025-03-31 14:38:28 +0900

    log in/out

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, CustomAuthenticationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 
 def signup(request):
     if request.method == 'POST':
@@ -14,3 +16,21 @@ def signup(request):
             'form': form,
     }
     return render(request, 'signup.html', context)
+
+def login(request):
+    if request.method == 'POST':
+        form = CustomAuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth_login(request, user)
+            return redirect('posts:index')
+    else:
+        form = CustomAuthenticationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'login.html', context)
+
+def logout(request):
+    auth_logout(request)
+    return redirect('posts:index')
